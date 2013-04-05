1a01fc02f15d5ffffb2c49fba4b2ed66ea918836
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2013-04-06 03:14:16 +0530

    Code Commit

---
+++ JqValidatingKoTemplates/Scripts/jqtemplatessample.js
@@ -0,0 +1,36 @@
+﻿var viewModel = {
+    collect: ko.observableArray([
+    {
+        position: "position0",
+        url: ko.observable("#"),
+        description: ko.observable("Some Description")
+    }]),
+}
+
+$(document).ready(function ()
+{
+    $.validator.messages.required = 'Required';
+
+    $(document).delegate("#addData", "click", null, function ()
+    {
+        viewModel.collect.push(
+       {
+           position: "position" + viewModel.collect().length,
+           url: ko.observable("#"),
+           description: ko.observable("Some Description" + viewModel.collect().length)
+       });
+        return false;
+    });
+
+    ko.applyBindings(viewModel);
+
+    $(document).delegate("#submitButton", "click", null, function ()
+    {
+        $.ajax({
+            url: "Home/Index",
+            contentType: "application/json",
+            dataType: "json",
+            data: {}
+        });
+    });
+});
